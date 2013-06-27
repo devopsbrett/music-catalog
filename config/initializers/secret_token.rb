e4b0f2756cb686a25ccb4fa0808d6c1ac244f4b1
Brett Mack <brett.mack@foxienet.com>
2013-06-27 04:02:55 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicCatalog::Application.config.secret_key_base = '189b84ddbb19b7df3b86c75de5f2e72b4b88330aa9d8d969276e7171b0ce94b7c853ffa1170a65aa7257b8f2c8d6202deb7656a060654a06fba866c381af58ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MusicCatalog::Application.config.session_store :cookie_store, key: '_music-catalog_session'
